8f5a636939b95973bfe67d1f4fd7205faca0d117
Ifylee <159982321+Ifylee@users.noreply.github.com>
2024-06-29 10:07:43 -0500

    added codes to my generateMarkdown file

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,20 +1,58 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// Function to generate a formatted markdown string for a README file based on the user's input data. 
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+  ${data.license}
 
+  ## Description
+  ${data.description}
+
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+
+  ## Installation
+  To install dependencies, run the following command:
+
+  \`\`\`
+  ${data.installation} 
+
+  \`\`\`
+
+  ## Usage
+  ${data.usage}
+  
+
+  ## License
+    This project is licensed under the ${data.license} license.
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  Run the following command for tests:
+  \`\`\`
+  ${data.test}
+  \`\`\`
+
+
+  ## Questions
+
+  For more questions, please open an issue on my github: [${data.github}](https://github.com/${data.github})
+
+  You can also contact me directly at: [${data.email}](mailto:${data.email})
+
+A walkthrough video of my application: [./image.png](https://drive.google.com/file/d/1Y3xW-hvpAtckOe-4lEeESHfdhUAhvibF/view?usp=sharing)
 `;
-}
+};
 
+// Exported for use in other parts of the application
 module.exports = generateMarkdown;
+
+
--- index.js
+++ index.js
@@ -54,3 +54,20 @@ const questions = [
         message: "What does the user need to know about contributing to the repo?"
     }
 ];
+
+// Function to write README file
+const writeToFile = (fileName, data) =>
+    fs.writeFileSync(path.join(process.cwd(), fileName), data);
+
+
+//Function to initialize the process of creating a README file based on user input
+   const init = () => {
+       inquirer.prompt(questions).then((responses) => {
+   // logs a message to indicate that the process of generating the README file is in process.
+           console.log("Generating README....");
+           writeToFile("README.md", generateMarkdown(responses));
+       });
+   };
+
+// Function call to initialize app
+init();
