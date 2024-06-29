550d8bd106562a01803b6e4984fc5d5ad07b0cce
Ifylee <159982321+Ifylee@users.noreply.github.com>
2024-06-29 10:03:24 -0500

    added codes to my index.js file

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
-// TODO: Include packages needed for this application
+// required modules
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require('./generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+// questions Array
+const questions = [
+    {
+        type: "input",
+        name: "github",
+        message: "What is your Github username?" 
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?" 
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project's name?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please write a short description of your project"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What kind of license should your project have?",
+        choices: ["MIT", "APACHE", "GPL", "None"]
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "what command should be run to install dependencies?"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What command should be run to run tests?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What does the user need to know about using the repo?"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "What does the user need to know about contributing to the repo?"
+    }
+];
